a474c356759912116fcd1e3e6c240c23c401375d
alwaystiys <365825701@qq.com>
2024-08-14 19:37:36 +0800

    add glad and test window

--- include/config.h
+++ include/config.h
@@ -1,6 +1,1 @@
 #include <iostream>
-
-void initConfig(){
-    std::cout << "Configuring..." << std::endl;
-    std::cout << "Configured!" << std::endl;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,42 @@
-#include "config.h"
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+//
+#include "config.h"
 
 int main()
 {
-    std::cout << "make ONE" << std::endl;
-    std::cout << "make TWO" << std::endl;
-    std::cout << "make THREE" << std::endl;
-    std::cout << "make FOUR" << std::endl;
-    std::cout << "make FIVE" << std::endl;
-    std::cout << "make SIX" << std::endl;
-    std::cout << "make GLFW" << std::endl;
-    std::cout << "make GLFW succ" << std::endl;
-    initConfig();
-    while (1);
+    if(!glfwInit())
+    {
+        std::cout << "glfw init failed" << std::endl;
+        return -1;
+    }
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Open3D", NULL, NULL);
+    if(!window){
+        std::cout << "window init failed" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    
+    gladLoadGL(glfwGetProcAddress);
+    // gladLoadGLLoader(glfwGetProcAddress);
+    glfwSwapInterval(1);
+
+    while(!glfwWindowShouldClose(window)){
+        
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     return 0;
 }
