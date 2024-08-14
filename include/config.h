b649b0a65d37c23bba1a8f23a78a2f86a9eabb74
alwaystiys <365825701@qq.com>
2024-08-14 17:07:50 +0800

    for test

---
+++ include/config.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void initConfig(){
+    std::cout << "Configuring..." << std::endl;
+    std::cout << "Configured!" << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "config.h"
+// #include <GLFW/glfw3.h>
+
+
+int main()
+{
+    std::cout << "make ONE" << std::endl;
+    std::cout << "make TWO" << std::endl;
+    std::cout << "make THREE" << std::endl;
+    std::cout << "make FOUR" << std::endl;
+    std::cout << "make FIVE" << std::endl;
+    std::cout << "make SIX" << std::endl;
+    std::cout << "make GLFW" << std::endl;
+    initConfig();
+    while (1);
+    return 0;
+}
